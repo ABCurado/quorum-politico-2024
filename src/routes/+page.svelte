688f4909e62940d19b003dac7bba543d148f053a
Antonio Curado <abestevao@gmail.com>
2023-12-31 17:51:43 +0000

    Add Svelte Share Buttons Component and update social media sharing buttons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
 	import { fade } from 'svelte/transition';
-	import { IconBrandFacebook, IconBrandX, IconBrandLinkedin,IconBrandWhatsapp } from '@tabler/icons-svelte';
+
 	import Welcome from './Welcome.svelte';
 	import Document from './Document.svelte';
 	import mixpanel from 'mixpanel-browser';
+	import SocialShare from './SocialShare.svelte';
 
 	export let data;
 	let readInstructions = false;
@@ -76,32 +77,13 @@
 			{/each}
 		</div>
 		<!-- Buttons to share the results in social media -->
-		<div class="flex flex-row justify-center space-x-4 m-8">
-			<a
-				href="https://twitter.com/intent/tweet?text=Descobre%20em%20quem%20votar%20nas%20pr%C3%B3ximas%20elei%C3%A7%C3%B5es%20com%20o%20Concordas%3F%20https%3A%2F%2Fconcordas.pt"
-				target="_blank"
-				class="bg-blue-400 hover:bg-blue-500 text-white font-bold py-3 px-6 rounded-full"
-			>
-				<IconBrandX />
-			</a>
-			<a href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fconcordas.pt" target="_blank" class="bg-blue-600 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded-full">
-				<IconBrandFacebook />
-			</a>
-			<a
-				href="https://www.linkedin.com/shareArticle?mini=true&url=https%3A%2F%2Fconcordas.pt&title=Descobre%20em%20quem%20votar%20nas%20pr%C3%B3ximas%20elei%C3%A7%C3%B5es%20com%20o%20Concordas%3F"
-				target="_blank"
-				class="bg-blue-800 hover:bg-blue-900 text-white font-bold py-3 px-6 rounded-full"
-			>
-				<IconBrandLinkedin />
-			</a>
-			<a
-				href="https://api.whatsapp.com/send?text=Descobre%20em%20quem%20votar%20nas%20pr%C3%B3ximas%20elei%C3%A7%C3%B5es%20com%20o%20Concordas%3F%20https%3A%2F%2Fconcordas.pt"
-				target="_blank"
-				class="bg-green-500 hover:bg-green-600 text-white font-bold py-3 px-6 rounded-full"
-			>
-				<IconBrandWhatsapp />
-			</a>
-
+		<div class="flex gap-2 justify-center w-full fixed bottom-4 center">
+			<SocialShare
+				class="flex justify-center space-x-4 m-8"
+				title="Concordas?"
+				url="https://em-quem-votar-2023.pages.dev/"
+				description="O partido mais próximo a ti é o: {calculateProximity()[0].party}"
+			/>
 		</div>
 		<div class="flex flex-col justify-center items-center mt-4 px-4 sm:px-0">
 			<p class="text-center text-base sm:text-lg mb-4">Se não concordas com o resultado, podes sempre voltar atrás e mudar o teu voto.</p>
---
+++ src/routes/SocialShare.svelte
@@ -0,0 +1,22 @@
+<script>
+	// @ts-ignore
+	import { Email, HackerNews, Reddit, LinkedIn, Pinterest, Telegram, Tumblr, Vk, WhatsApp, Xing, Facebook, X, Line } from 'svelte-share-buttons-component';
+
+	export let url = 'https://pchynoweth.github.io/svelte-share-buttons-component/';
+	export let title = 'Svelte Share Buttons Component';
+	export let desc = 'Svelte based social media share buttons component with no tracking.';
+</script>
+
+<!-- <Email subject={title} body="{desc} {url}" /> -->
+<!-- <HackerNews class="share-button" {title} {url} /> -->
+<!-- <Reddit class="share-button" {title} {url} /> -->
+<LinkedIn class="share-button rounded" {url} />
+<!-- <Tumblr class="share-button" {title} {url} caption={title} /> -->
+<!-- <Pinterest class="share-button" {url} media="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Svelte_Logo.svg/200px-Svelte_Logo.svg.png" description={title} /> -->
+<Telegram class="share-button rounded" text={title} {url} />
+<!-- <Vk class="share-button" {title} {url} /> -->
+<WhatsApp class="share-button rounded" text="{title} {url}" />
+<!-- <Xing class="share-button" {title} {url} /> -->
+<Facebook class="share-button rounded" quote={title} {url} />
+<X class="share-button rounded" text={title} {url} hashtags="github,svelte" via="username" related="other,users" />
+<!-- <Line class="share-button" {url} /> -->
