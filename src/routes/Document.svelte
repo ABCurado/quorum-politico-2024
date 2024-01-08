5127cecaf0ad73d32acd5583a1de1a860a69b93d
Antonio Curado <abestevao@gmail.com>
2024-01-08 22:30:27 +0100

    Update Document.svelte with min-height and max-height properties

--- src/routes/Document.svelte
+++ src/routes/Document.svelte
@@ -6,7 +6,7 @@
 
 <div class="flex flex-col items-center justify-center p-4 sm:p-0">
 	<div class="max-w-screen-sm mx-auto">
-		<h3 class="text-xl sm:text-l font-bold mb-4 text-center">{title}</h3>
+		<h3 class="text-xl sm:text-l min-h-32 font-bold mb-4 text-center">{title}</h3>
 	</div>
 	<!-- Display the tags from the document in a nice way -->
 	<!-- <div class="flex flex-row flex-wrap justify-center mb-4">
@@ -17,7 +17,7 @@
 		{/each}
 	</div> -->
 	<textarea
-		class="text-base sm:text-lg mb-2 h-48 sm:h-64 lg:h-96 w-full max-w-xl p-4 border rounded-lg bg-gray-100"
+		class="text-base sm:text-lg mb-2 min-h-96 w-full max-w-xl p-4 border rounded-lg bg-gray-100"
 		value={summary}
 		readonly
 	/>
