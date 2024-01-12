23c99735b9cc07df8445e89221b47177dada607c
Antonio Curado <abestevao@gmail.com>
2024-01-12 20:21:41 +0100

    Refactor Hemicycle component and update Welcome page

--- src/routes/Hemicycle.svelte
+++ src/routes/Hemicycle.svelte
@@ -12,22 +12,33 @@
 		CH: 'fill-current text-blue-600'
 	};
 	let opacity = 1.0;
-	let radius = 7;
 
+	export let randomRadius = false;
 	export let centerText = '';
 	export let partyRankingList: { party: string; proximity: number }[] = [];
+
 	let partyRanking = Object.fromEntries(partyRankingList.map((party) => [party.party, party.proximity]));
 
+	function calculateRadius(party: number | undefined) {
+		let defaultRadiusBase = 6;
+		if (party) {
+			return defaultRadiusBase * (partyRanking[party] || 0.8);
+		} else if (randomRadius) {
+			return defaultRadiusBase * Math.random();
+		} else {
+			return defaultRadiusBase;
+		}
+	}
 </script>
 
 <div class="flex flex-col justify-center items-center">
-	<svg class="w-full" viewBox="0 0 360 185">
+	<svg class="w-full" viewBox="0 0 360 185" preserveAspectRatio="xMidYMid meet" height={window.innerWidth >= 768 ? 500 : 200}>
 		<!-- Created with the Wikimedia parliament diagram creator (http://tools.wmflabs.org/parliamentdiagram/parliamentinputform.html) -->
 		<g>
 			<text x="175" y="175" class="text-3xl font-bold text-center" style="text-anchor:middle;"> {centerText} </text>
 
 			{#each parlimentData as seat}
-				<circle cx={seat[0]} cy={seat[1]} r={radius * partyRanking[seat[2]]} class={partyColors[seat[2]]} style="opacity: {partyRanking[seat[2]] || { opacity }}" />
+				<circle cx={seat[0]} cy={seat[1]} r={calculateRadius(partyRanking[seat[2]])} class={partyColors[seat[2]]} style="opacity: {partyRanking[seat[2]] || opacity};" />
 			{/each}
 		</g>
 	</svg>
--- src/routes/Welcome.svelte
+++ src/routes/Welcome.svelte
@@ -1,25 +1,47 @@
 <script lang="ts">
 	export let readInstructions = false;
+	import Hemicycle from './Hemicycle.svelte';
+	import { onMount } from 'svelte';
+
+	let ticker = 0;
+
+	onMount(() => {
+		const interval = setInterval(() => {
+			ticker++;
+		}, 3000);
+
+		return () => {
+			clearInterval(interval);
+		};
+	});
 </script>
 
 {#if !readInstructions}
 	<div class="flex flex-col items-center p-6 sm:p-8">
-		<h1 class="text-center text-4xl mb-4">Descobre em quem votar</h1>
-		<p class="text-center text sm:text-base mb-4">
-			Olá! Bem-vindo ao <strong>Concordas?</strong>, um jogo que te permite descobrir em quem votar nas próximas eleições. O jogo é baseado nas votaçõoes que aconteceram no parlamento europeu de 2022-2023.
-		</p>
+		<!-- <h1 class="text-center text-4xl mb-4">Descobre em quem votar</h1> -->
+		<!--Olá! Bem-vindo ao <strong>Concordas?</strong>, um jogo que te permite descobrir em quem votar nas próximas eleições. O jogo é baseado nas votaçõoes que aconteceram no parlamento europeu de 2022-2023. -->
+
+		<h1 class="text-center text-4xl mb-4">Descobre que partido melhor te representa!</h1>
+		<p class="text-center text sm:text-base mb-4">Bem-vindo! Este simulador permite-te descobrir como votaram os partidos em algumas das Propostas apresentadas na Assembleia da República.</p>
+		{#key ticker}
+			<Hemicycle centerText="Decide" randomRadius={true} />
+		{/key}
+
 		<div class="mt-4 mb-8">
-			<ul class="list-disc list-inside ml-5 text-slate-900 dark:text-slate-200 list-image-[url('data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTQiIGhlaWdodD0iMTIiIHZpZXdCb3g9IjAgMCAxNCAxMiIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiBmaWxsPSIjMzhiZGY4Ij48cGF0aCBmaWxsLXJ1bGU9ImV2ZW5vZGQiIGQ9Ik0xMy42ODUuMTUzYS43NTIuNzUyIDAgMCAxIC4xNDMgMS4wNTJsLTggMTAuNWEuNzUuNzUgMCAwIDEtMS4xMjcuMDc1bC00LjUtNC41YS43NS43NSAwIDAgMSAxLjA2LTEuMDZsMy44OTQgMy44OTMgNy40OC05LjgxN2EuNzUuNzUgMCAwIDEgMS4wNS0uMTQzWiIgLz48L3N2Zz4=')]">
-				<li>Lê a proposta que te é apresentada</li> 
-				<li>Decide se concordas ou não com o mesmo.</li>
-				<li>No final, o jogo vai-te dizer em quem votar.</li>
+			As <b>instruções</b> são simples:
+			<ul
+				class="list-disc list-inside ml-5 text-slate-900 dark:text-slate-200 list-image-[url('data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTQiIGhlaWdodD0iMTIiIHZpZXdCb3g9IjAgMCAxNCAxMiIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiBmaWxsPSIjMzhiZGY4Ij48cGF0aCBmaWxsLXJ1bGU9ImV2ZW5vZGQiIGQ9Ik0xMy42ODUuMTUzYS43NTIuNzUyIDAgMCAxIC4xNDMgMS4wNTJsLTggMTAuNWEuNzUuNzUgMCAwIDEtMS4xMjcuMDc1bC00LjUtNC41YS43NS43NSAwIDAgMSAxLjA2LTEuMDZsMy44OTQgMy44OTMgNy40OC05LjgxN2EuNzUuNzUgMCAwIDEgMS4wNS0uMTQzWiIgLz48L3N2Zz4=')]"
+			>
+				<li>Lê a proposta que te é apresentada, sumário e contra argumentos.</li>
+				<li>Usa os poderes do partido: decide se votarias a favor, contra, ou se te irias abster.</li>
+				<li>No final, o jogo vai-te dizer qual o partido com que mais te assemelhas.</li>
 			</ul>
 		</div>
 		<button
-			class="bg-green-500 hover:bg-green-700 text-white font-bold py-3 px-3 sm:px-4 rounded-full"
+			class="bg-green-400 hover:bg-green-700 text-white font-bold py-3 px-3 sm:px-4 rounded-full"
 			on:click={() => {
 				readInstructions = true;
-			}}>Começar</button
+			}}>Estás preparado para ser <b><i>Deputado por 5 minutos</i></b>?</button
 		>
 	</div>
 {/if}
