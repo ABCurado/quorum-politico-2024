75152e1d3c0476d348c62f552887ff449c73673a
Antonio Curado <abestevao@gmail.com>
2024-01-13 17:19:20 +0100

    Fix environment variable in DevBanner component

--- src/routes/DevBanner.svelte
+++ src/routes/DevBanner.svelte
@@ -1,7 +1,7 @@
-<script>
+<!-- <script>
 	import { ENV } from '$env/static/private';
-</script>
+</script> -->
 
-{#if ENV === 'dev'}
+{#if env.ENV === 'dev'}
 	<div class="bg-red-500 text-white p-4">Development Environment</div>
 {/if}
