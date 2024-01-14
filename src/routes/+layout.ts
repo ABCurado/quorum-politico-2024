b826b8c4d12b5f5b5a632343b752f363fa02a170
Antonio Curado <abestevao@gmail.com>
2024-01-14 17:14:09 +0100

    Add mixpanel configuration and environment property

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,11 +1,24 @@
 import mixpanel from 'mixpanel-browser';
 import { browser } from '$app/environment';
 
+
 export const load = async () => {
+	type Config = {
+		host: string;
+		track_pageview: boolean;
+	};
+
 	if (browser) {
 		mixpanel.init("e611f2810e5ef94ed59f38783c6b6ff9", {
 			host: "api-eu.mixpanel.com",
 			track_pageview: true
+		} as Config);
+
+		// Set the environment property
+		mixpanel.set_config({
+			properties: {
+				environment: platform?.env.ENV
+			}
 		});
 	}
 	return
