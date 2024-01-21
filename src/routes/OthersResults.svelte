016be270b511bba251687647b200e704a23ecf29
Antonio Curado <abestevao@gmail.com>
2024-01-21 16:15:37 +0100

    Refactor voting logic and UI

--- src/routes/OthersResults.svelte
+++ src/routes/OthersResults.svelte
@@ -1,10 +1,19 @@
 <script lang="ts">
 	import mixpanel from 'mixpanel-browser';
 
-	let voteResults: { agrees: number; disagrees: number } = {
-		agrees: 0,
-		disagrees: 0
+	// agrees: 1 - Means agree
+	// agrees: 0 - Means disagree
+	// aggres: 2 - Means no vote
+	type Vote = {
+		agrees: number;
+		votes: number;
 	};
+
+	type Votes = {
+		[key: string]: Vote[];
+	};
+
+	let voteResults: Votes = {};
 	let showResults = false;
 	let loading = false;
 
@@ -35,7 +44,7 @@
 				agrees: event.target.id === '1' ? 1 : 0
 			})
 		});
-		const results = await fetchData();
+		const voteResults = await fetchData();
 		loading = false;
 		showResults = true;
 	}
@@ -57,26 +66,50 @@
 		<div class="animate-spin rounded-full h-32 w-32 border-b-2 border-gray-900" />
 	</div>
 {:else if showResults}
-	<div class="flex justify-center">
-		<div class="w-1/2">
-			<div class="flex justify-between">
-				<div class="text-center text-xl font-bold text-green-500">{voteResults.agrees}</div>
-				<div class="text-center text-xl font-bold text-red-500">{voteResults.disagrees}</div>
-			</div>
-			<div class="flex justify-between">
-				<div class="text-center text-xl font-bold text-green-500">Concordam</div>
-				<div class="text-center text-xl font-bold text-red-500">Não concordam</div>
-			</div>
-			<div class="flex justify-center">
-				<div class="w-1/2">
-					<div class="bg-green-300 h-4 rounded-full mb-2">
-						<div class="w-1/2 bg-green-500 h-4 rounded-full" />
-					</div>
-					<div class="bg-red-300 h-4 rounded-full mb-2">
-						<div class="w-1/2 bg-red-500 h-4 rounded-full" />
-					</div>
+	<div class="container mx-auto px-4">
+		{#each Object.keys(voteResults) as party}
+			<div class="shadow overflow-hidden sm:rounded-lg mb-4 w-full sm:w-3/4">
+				<div class="px-4 py-5 sm:px-6 border-b border-gray-200 w-full">
+					<h2 class="text-lg leading-6 font-medium text-gray-900">{party}</h2>
+				</div>
+
+				<div class="overflow-x-auto">
+					<table class="min-w-full divide-y divide-gray-200 shadow mb-4 w-full sm:w-3/4 mx-auto">
+						<thead class="bg-gray-50">
+							<tr>
+								<th class="px-2 sm:px-6 py-3 text-xs sm:text-sm font-medium text-gray-500 uppercase tracking-wider">Voto</th>
+								<th class="px-2 sm:px-6 py-3 text-xs sm:text-sm font-medium text-gray-500 uppercase tracking-wider">Resultado votaçāo final</th>
+							</tr></thead
+						>
+						<tbody class="bg-white divide-y divide-gray-200">
+							<tr>
+								<td class="px-2 sm:px-6 py-2 sm:py-4 whitespace-nowrap text-xs sm:text-sm text-gray-500 text-center"
+									>{voteResults[party][0].agrees === 0 ? '❌' : voteResults[party][0].agrees === 1 ? '✅' : '🤷‍♂️'}</td
+								>
+								<td class="px-2 sm:px-6 py-2 sm:py-4 whitespace-nowrap text-xs sm:text-sm text-gray-500 text-center"
+									>{voteResults[party][0].votes === 0 ? '❌' : voteResults[party][0].votes === 1 ? '✅' : '🤷‍♂️'}</td
+								>
+							</tr>
+						</tbody>
+					</table>
+					<table class="min-w-full divide-y divide-gray-200 shadow mb-4 w-full sm:w-3/4 mx-auto">
+						<thead class="bg-gray-50">
+							<tr>
+								{#each Object.keys(voteResults[party][1]) as party}
+									<th class="px-2 sm:px-6 py-3 text-xs sm:text-sm font-medium text-gray-500 uppercase tracking-wider">{party}</th>
+								{/each}
+							</tr>
+						</thead>
+						<tbody class="bg-white divide-y divide-gray-200">
+							<tr>
+								{#each Object.values(voteResults[party][1]) as result}
+									<td class="px-2 sm:px-6 py-2 sm:py-4 whitespace-nowrap text-xs sm:text-sm text-gray-500 text-center">{result == 0 ? '❌' : result == 1 ? '✅' : '🤷‍♂️'}</td>
+								{/each}
+							</tr>
+						</tbody>
+					</table>
 				</div>
 			</div>
-		</div>
+		{/each}
 	</div>
 {/if}
--- src/routes/votes/+server.ts
+++ src/routes/votes/+server.ts
@@ -1,4 +1,4 @@
-import type { RequestHandler } from "@sveltejs/kit";
+import type { RequestHandler } from '@sveltejs/kit';
 
 // ┌─────┬───────────┬─────────┬─────────┬────────────┬────┐
 // │ cid │ name      │ type    │ notnull │ dflt_value │ pk │
@@ -18,87 +18,79 @@ import type { RequestHandler } from "@sveltejs/kit";
 // │ 6   │ _updated  │ TEXT    │ 0       │            │ 0  │
 // └─────┴───────────┴─────────┴─────────┴────────────┴────┘
 interface Vote {
-    device_id: number;
-    env: string;
-    results: Buffer;
-    top_party: string;
-    agrees: number;
-    _created: string;
-    _updated: string;
+	device_id: number;
+	env: string;
+	results: Buffer;
+	top_party: string;
+	agrees: number;
+	_created: string;
+	_updated: string;
 }
 
 /** @type {import('@sveltejs/kit').RequestHandler} */
 export const GET: RequestHandler = async ({ request, platform }) => {
-    let result = await platform?.env.DB.prepare(
-        `
+	let result = await platform?.env.DB.prepare(
+		`
         SELECT 
             top_party, 
             agrees,
             COUNT(*) AS votes 
         FROM votes 
         GROUP BY 1 ,2
-        `).run();
-    const transformedResult = result.results.reduce((acc: any, row: any) => {
-        const { top_party, agrees, votes } = row;
-        if (!acc[top_party]) {
-            acc[top_party] = [];
-        }
-        acc[top_party].push({ agrees, votes });
-        return acc;
-    }, {});
+        `
+	).run();
+	const transformedResult = result.results.reduce((acc: any, row: any) => {
+		const { top_party, agrees, votes } = row;
+		if (!acc[top_party]) {
+			acc[top_party] = [];
+		}
+		acc[top_party].push({ agrees, votes });
+		return acc;
+	}, {});
 
-    return new Response(JSON.stringify(transformedResult));
-}
+	return new Response(JSON.stringify(transformedResult), { headers: { 'Content-Type': 'application/json' } });
+};
 
 /** @type {import('@sveltejs/kit').RequestHandler} */
 export const POST: RequestHandler = async ({ request, platform }) => {
-    try {
-        let body = await request.json();
+	try {
+		let body = await request.json();
 
-        // Input validation
-        if (!body.device_id || !body.results || !body.top_party) {
-            return new Response('Invalid input', { status: 400 });
-        }
+		// Input validation
+		if (!body.device_id || !body.results || !body.top_party) {
+			return new Response('Invalid input', { status: 400 });
+		}
 
-        // Rate limiting
-        // const lastInsertTime = await platform?.env.CACHE.get(body.device_id);
-        // if (lastInsertTime) {
-        //     return new Response('Too many requests, please try again later.', { status: 429 });
-        // }
+		// Rate limiting
+		// const lastInsertTime = await platform?.env.CACHE.get(body.device_id);
+		// if (lastInsertTime) {
+		//     return new Response('Too many requests, please try again later.', { status: 429 });
+		// }
 
-        let result = await platform?.env.DB.prepare(
-            "INSERT INTO votes (device_id, env, results, top_party, agrees, _created, _updated) VALUES (?1, ?2, ?3, ?4, ?5, ?6, ?7)"
-        ).bind(
-            body.device_id,
-            platform?.env.ENV,
-            JSON.stringify(body.results),
-            body.top_party,
-            2,
-            new Date().toISOString(),
-            new Date().toISOString()
-        ).run();
+		let result = await platform?.env.DB.prepare('INSERT INTO votes (device_id, env, results, top_party, agrees, _created, _updated) VALUES (?1, ?2, ?3, ?4, ?5, ?6, ?7)')
+			.bind(body.device_id, platform?.env.ENV, JSON.stringify(body.results), body.top_party, 2, new Date().toISOString(), new Date().toISOString())
+			.run();
 
-        // Store the timestamp of the last insert for this device id
-        // await platform?.env.CACHE.put(body.device_id, Date.now(), { expirationTtl: 5 * 60 });
+		// Store the timestamp of the last insert for this device id
+		// await platform?.env.CACHE.put(body.device_id, Date.now(), { expirationTtl: 5 * 60 });
 
-        return new Response(result, { status: 200 });
-    } catch (error) {
-        console.error(error);
-        return new Response(null, { status: 500 });
-    }
-}
+		return new Response(result, { status: 200 });
+	} catch (error) {
+		console.error(error);
+		return new Response(null, { status: 500 });
+	}
+};
 
 /** @type {import('@sveltejs/kit').RequestHandler} */
 export const PUT: RequestHandler = async ({ request, platform }) => {
-    try {
-        let body = await request.json();
-        let result = await platform?.env.DB.prepare(
-            "UPDATE votes SET agrees = ?1, _updated = ?2 WHERE rowid = (SELECT rowid FROM votes WHERE device_id = ?3 AND env = ?4 ORDER BY _updated DESC LIMIT 1)"
-        ).bind(body.agrees, new Date().toISOString(), body.device_id, platform?.env.ENV
-        ).run();
-        return new Response(null, { status: 200 });
-    } catch (error) {
-        console.error(error);
-        return new Response(null, { status: 500 });
-    }
-}
+	try {
+		let body = await request.json();
+		let result = await platform?.env.DB.prepare('UPDATE votes SET agrees = ?1, _updated = ?2 WHERE rowid = (SELECT rowid FROM votes WHERE device_id = ?3 AND env = ?4 ORDER BY _updated DESC LIMIT 1)')
+			.bind(body.agrees, new Date().toISOString(), body.device_id, platform?.env.ENV)
+			.run();
+		return new Response(null, { status: 200 });
+	} catch (error) {
+		console.error(error);
+		return new Response(null, { status: 500 });
+	}
+};
