b5171de1f4747642e7c351767f167d15218378a5
Antonio Curado <abestevao@gmail.com>
2024-01-21 15:12:04 +0100

    Refactor VoteResults.svelte component

--- src/routes/VoteResults.svelte
+++ src/routes/VoteResults.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
-	import mixpanel from "mixpanel-browser";
+	import mixpanel from 'mixpanel-browser';
 
 	export let vote_proposals: Array<{ party: string; proximity: number; title: string; author: string; final_result: string; user_vote: string }>;
 	let showResults = false;
 
 	$: if (vote_proposals) {
-		mixpanel.track("Click Vote Results", { vote_proposals });
+		mixpanel.track('Click Vote Results', { vote_proposals });
 	}
 </script>
 
@@ -15,15 +15,27 @@
 	{#each vote_proposals as vote}
 		<div class="shadow overflow-hidden sm:rounded-lg mb-4 w-full sm:w-3/4">
 			<div class="px-4 py-5 sm:px-6 border-b border-gray-200 w-full">
-				<h2 class="text-lg leading-6 font-medium text-gray-900">{vote.title} - {vote.author}</h2>
+				<h2 class="text-lg leading-6 font-medium text-gray-900">({vote.author}) {vote.title}</h2>
 			</div>
 
 			<div class="overflow-x-auto">
 				<table class="min-w-full divide-y divide-gray-200 shadow mb-4 w-full sm:w-3/4 mx-auto">
 					<thead class="bg-gray-50">
 						<tr>
-							<th class="px-2 sm:px-6 py-3 text-xs sm:text-sm font-medium text-gray-500 uppercase tracking-wider">Tu</th>
-							<th class="px-2 sm:px-6 py-3 text-xs sm:text-sm font-medium text-gray-500 uppercase tracking-wider">Resultado</th>
+							<th class="px-2 sm:px-6 py-3 text-xs sm:text-sm font-medium text-gray-500 uppercase tracking-wider">Voto</th>
+							<th class="px-2 sm:px-6 py-3 text-xs sm:text-sm font-medium text-gray-500 uppercase tracking-wider">Resultado votaçāo final</th>
+						</tr></thead
+					>
+					<tbody class="bg-white divide-y divide-gray-200">
+						<tr>
+							<td class="px-2 sm:px-6 py-2 sm:py-4 whitespace-nowrap text-xs sm:text-sm text-gray-500 text-center">{vote.user_vote === '0' ? '❌' : vote.user_vote === '1' ? '✅' : '🤷‍♂️'}</td>
+							<td class="px-2 sm:px-6 py-2 sm:py-4 whitespace-nowrap text-xs sm:text-sm text-gray-500 text-center">{vote.final_result === '0' ? '❌' : vote.final_result === '1' ? '✅' : '🤷‍♂️'}</td>
+						</tr>
+					</tbody>
+				</table>
+				<table class="min-w-full divide-y divide-gray-200 shadow mb-4 w-full sm:w-3/4 mx-auto">
+					<thead class="bg-gray-50">
+						<tr>
 							{#each Object.keys(vote.votes) as party}
 								<th class="px-2 sm:px-6 py-3 text-xs sm:text-sm font-medium text-gray-500 uppercase tracking-wider">{party}</th>
 							{/each}
@@ -31,8 +43,6 @@
 					</thead>
 					<tbody class="bg-white divide-y divide-gray-200">
 						<tr>
-							<td class="px-2 sm:px-6 py-2 sm:py-4 whitespace-nowrap text-xs sm:text-sm text-gray-500 text-center">{vote.user_vote === '0' ? '❌' : vote.user_vote === '1' ? '✅' : '🤷‍♂️'}</td>
-							<td class="px-2 sm:px-6 py-2 sm:py-4 whitespace-nowrap text-xs sm:text-sm text-gray-500 text-center">{vote.final_result === '0' ? '❌' : vote.final_result === '1' ? '✅' : '🤷‍♂️'}</td>
 							{#each Object.values(vote.votes) as result}
 								<td class="px-2 sm:px-6 py-2 sm:py-4 whitespace-nowrap text-xs sm:text-sm text-gray-500 text-center">{result == '0' ? '❌' : result == '1' ? '✅' : '🤷‍♂️'}</td>
 							{/each}
