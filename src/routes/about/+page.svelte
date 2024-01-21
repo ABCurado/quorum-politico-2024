9c59407fef047ce183693f739eaedb7a315becef
Antonio Curado <abestevao@gmail.com>
2024-01-21 15:18:11 +0100

    Add about page with project information

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+
+</script>
+
+<main class="min-h-screen bg-gray-100 dark:bg-gray-900 py-6 flex flex-col justify-center sm:py-12">
+    <div class="relative py-3 sm:max-w-xl sm:mx-auto">
+        <div class="absolute inset-0 bg-gradient-to-r from-cyan-400 to-light-blue-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
+        <div class="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20 dark:bg-gray-800">
+            <h1 class="text-2xl font-bold mb-2 text-gray-900 dark:text-white">Ainda precisamos de definir o nome</h1>
+            <p class="text-gray-700 dark:text-gray-400 mb-6">
+                O projeto ainda está em desenvolvimento, mas podes ver o progresso no <a href="">Github</a> ou no <a href="">Figma</a>.
+            </p>
+            <div class="flex items-center justify-center">
+                <a href="" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded-full mr-6">Github</a>
+                <a href="" class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-3 px-6 rounded-full">Figma</a>
+        </div>
+    </div>
+</main>
