9f1f4a6b61211adf1170601a0439f20e6efdd2b6
Antonio Curado <abestevao@gmail.com>
2024-01-27 19:24:05 +0100

    Refactor social sharing functionality

--- src/routes/SocialShare.svelte
+++ src/routes/SocialShare.svelte
@@ -6,50 +6,38 @@
 	export let desc = '';
 	export let hashtags = 'em-quem-votar-2023';
 
-	let textContent;
+	let supportsNavigatorShare = window.navigator.canShare === undefined ? false : true;
 
-	$: console.log('textContent', textContent);
 	async function navigatorShare() {
-		console.log('navigatorShare');
-		// feature detecting navigator.canShare() also implies
-		// the same for the navigator.share()
-		if (!window.navigator.canShare) {
-			textContent = `Your browser doesn't support the Web Share API.`;
-			return;
-		}
 		let response = await fetch("https://dev.em-quem-votar-2023.pages.dev/example.png")
 		let blob = await response.blob();
 		var file = new File([blob], "example.png", {type: 'image/png'});
-		var filesArray = [file];
-		try {
-			await window.navigator.share({
-				title: title,
-				files: filesArray,
-				text: 'Beautiful images',
-				url: url
-			});
-			textContent = 'Shared!';
-		} catch (error) {
-			textContent = `Error: ${error.message}`;
-		}
 
+		var filesArray = [file];
+		await window.navigator.share({
+			title: title,
+			files: filesArray,
+			text: 'Beautiful images',
+			url: url
+		});
 	}
 </script>
 
-<!-- <Email subject={title} body="{desc} {url}" /> -->
-<!-- <HackerNews class="share-button" {title} {url} /> -->
-<!-- <Reddit class="share-button" {title} {url} /> -->
-<LinkedIn class="share-button rounded" {url} />
-<!-- <Tumblr class="share-button" {title} {url} caption={title} /> -->
-<!-- <Pinterest class="share-button" {url} media="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Svelte_Logo.svg/200px-Svelte_Logo.svg.png" description={title} /> -->
-<Telegram class="share-button rounded" text={title} - {desc} {url} />
-<!-- <Vk class="share-button" {title} {url} /> -->
-<WhatsApp class="share-button rounded" text="{title} {url}" />
-<!-- <Xing class="share-button" {title} {url} /> -->
-<Facebook class="share-button rounded" quote={title} {url} />
-<X class="share-button rounded" text={title} {desc} {url} related="other,users" {hashtags} />
-<!-- <Line class="share-button" {url} /> -->
-<button class="share-button rounded" on:click={() => window.navigator.clipboard.writeText(url)}>Copy Link</button>
-<button class="share-button rounded" on:click={() => {console.log("KKKK");navigatorShare()}}>Navigator Share</button>
-<br/>
-{@html textContent}
+{#if supportsNavigatorShare}
+	<button class="share-button rounded" on:click={() => {navigatorShare()}}>Navigator Share</button>
+{:else}
+	<!-- <Email subject={title} body="{desc} {url}" /> -->
+	<!-- <HackerNews class="share-button" {title} {url} /> -->
+	<!-- <Reddit class="share-button" {title} {url} /> -->
+	<LinkedIn class="share-button rounded" {url} />
+	<!-- <Tumblr class="share-button" {title} {url} caption={title} /> -->
+	<!-- <Pinterest class="share-button" {url} media="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Svelte_Logo.svg/200px-Svelte_Logo.svg.png" description={title} /> -->
+	<Telegram class="share-button rounded" text={title} - {desc} {url} />
+	<!-- <Vk class="share-button" {title} {url} /> -->
+	<WhatsApp class="share-button rounded" text="{title} {url}" />
+	<!-- <Xing class="share-button" {title} {url} /> -->
+	<Facebook class="share-button rounded" quote={title} {url} />
+	<X class="share-button rounded" text={title} {desc} {url} related="other,users" {hashtags} />
+	<!-- <Line class="share-button" {url} /> -->
+	<!-- <button class="share-button rounded" on:click={() => window.navigator.clipboard.writeText(url)}>Copy Link</button> -->
+{/if}
