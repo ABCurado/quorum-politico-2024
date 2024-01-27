a3a698498f62d03511eca32a24638e0a7df4f433
Antonio Curado <abestevao@gmail.com>
2024-01-27 17:22:50 +0100

    Update BarChart and PartyInfo components

--- src/routes/party/PartyInfo.svelte
+++ src/routes/party/PartyInfo.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
-    import { onMount } from 'svelte';
-    import { Modal } from 'flowbite-svelte';
-    import type { PartyInfo } from './+server.ts';
-    
+	import { onMount } from 'svelte';
+	import { Modal } from 'flowbite-svelte';
+	import type { PartyInfo } from './+server.ts';
+
 	export let party: string;
 	export let show = false;
 	let info: PartyInfo;
@@ -17,15 +17,14 @@
 </script>
 
 <Modal title={party} bind:open={show} autoclose outsideclose>
-
-{#each info.proposals as proposal}
-    <div class="shadow overflow-hidden sm:rounded-lg mb-4 w-full">
-        <div class="px-4 py-5 sm:px-6 border-b border-gray-200 w-full">
-            <h2 class="text leading-6 font-medium text-gray-700">{proposal}</h2>
-        </div>
-    </div>
-{/each}
+	{#each info.proposals as proposal}
+		<div class="mb-4 w-full overflow-hidden shadow sm:rounded-lg">
+			<div class="w-full border-b border-gray-200 px-4 py-5 sm:px-6">
+				<h2 class="text font-medium leading-6 text-gray-700">{proposal}</h2>
+			</div>
+		</div>
+	{/each}
 	<svelte:fragment slot="footer">
-    <a href={info.link} target="_blank">Link para as propostas</a>
-</svelte:fragment>
+		<a href={info.link} target="_blank" class="text-blue-500 hover:underline">Link para as propostas</a>
+	</svelte:fragment>
 </Modal>
