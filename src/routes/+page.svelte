838dac0e95fcc389460383d9f6b3949044fe1334
Antonio Curado <abestevao@gmail.com>
2024-01-27 17:26:54 +0100

    Update party information button text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -133,7 +133,7 @@
 					showPartyInfo = true;
 				}
 			}}
-		>Mais informação sobre o partido aqui
+		>Descobre mais sobre o teu partido
 		</button>
 		<PartyInfo bind:show={showPartyInfo} party={proximity[0].party} />
 		<BarChart {proximity} />
