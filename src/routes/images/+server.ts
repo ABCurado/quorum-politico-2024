2998cd62b3ceb515e55b40ca64694d35dea5ef9b
Antonio Curado <abestevao@gmail.com>
2024-01-28 16:40:44 +0100

    Add resvg-wasm and svg2png-wasm dependencies

--- src/routes/images/+server.ts
+++ src/routes/images/+server.ts
@@ -1,13 +1,19 @@
+import * as resvg from '@resvg/resvg-wasm';
+
+import Document from '../Document.svelte';
 import Hemicycle from '../Hemicycle.svelte';
 import type { RequestHandler } from '@sveltejs/kit';
-import { Resvg } from '@resvg/resvg-js';
+import db from '../proposals/proposals_db.json';
 import { html } from 'satori-html';
+// @ts-ignore
 import satori from 'satori';
 
 /** @type {import('@sveltejs/kit').RequestHandler} */
 export const GET: RequestHandler = async ({ url }) => {
-	const result = Hemicycle.render({random:true});
+	const result = Hemicycle.render({ ...db[0], random: true });
 	const markup = html(`${result.html}<style>${result.css.code}</style>`);
+	await fetch('https://dev.em-quem-votar-2023.pages.dev/resvg_bg.wasm?module').then((res) => resvg.initWasm(res))
+
 	const svg = await satori(markup, {
 		fonts: [
 			{
@@ -19,19 +25,24 @@ export const GET: RequestHandler = async ({ url }) => {
 		height: 500,
 		width: 500
 	});
-
-	const resvg = new Resvg(svg, {
-		fitTo: {
-			mode: 'width',
-			value: 500
-		}
-	});
-
-	const png = resvg.render();
-
-	return new Response(svg, {
-		headers: {
-			'content-type': 'image/svg+xml'
-		}
-	});
+	try {
+		const resvgJS = new resvg.Resvg(svg, {
+			fitTo: {
+				mode: 'width',
+				value: 1280
+			}
+		});
+		const pngData = resvgJS.render();
+		const pngBuffer = pngData.asPng();
+		return new Response(pngBuffer, {
+			headers: {
+				'content-type': 'image/png'
+			}
+		});
+	} catch (error) {
+		console.error(error);
+		return new Response(error.message, {
+			status: 500
+		});
+	}
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,14 @@
-import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { sveltekit } from '@sveltejs/kit/vite';
+import topLevelAwait from 'vite-plugin-top-level-await';
+import wasm from 'vite-plugin-wasm';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit(), wasm()],
+	worker: {
+		// Not needed with vite-plugin-top-level-await >= 1.3.0
+		// format: "es",
+		plugins: [wasm(), topLevelAwait()]
+	},
+	assetsInclude: ["**/*.wasm"],
 });
