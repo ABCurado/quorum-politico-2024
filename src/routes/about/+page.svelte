221b6943ef19ccc69f55b9a419ac587bed0a351e
Antonio Curado <abestevao@gmail.com>
2024-02-02 21:48:32 +0100

    Add Hemicycle component and update content in about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,18 +1,71 @@
 <script>
-
+	import Hemicycle from '../Hemicycle.svelte';
 </script>
 
-<main class="min-h-screen bg-gray-100 dark:bg-gray-900 py-6 flex flex-col justify-center sm:py-12">
-    <div class="relative py-3 sm:max-w-xl sm:mx-auto">
-        <div class="absolute inset-0 bg-gradient-to-r from-cyan-400 to-light-blue-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
-        <div class="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20 dark:bg-gray-800">
-            <h1 class="text-2xl font-bold mb-2 text-gray-900 dark:text-white">Ainda precisamos de definir o nome</h1>
-            <p class="text-gray-700 dark:text-gray-400 mb-6">
-                O projeto ainda está em desenvolvimento, mas podes ver o progresso no <a href="">Github</a> ou no <a href="">Figma</a>.
-            </p>
-            <div class="flex items-center justify-center">
-                <a href="" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded mr-6">Github</a>
-                <a href="" class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-3 px-6 rounded">Figma</a>
-        </div>
-    </div>
-</main>
+<div class="flex w-full justify-center">
+	<Hemicycle random={true} />
+</div>
+<div class="relative mx-auto w-10/12 space-y-8 py-3 sm:mx-auto">
+	<div class="space-y-4">
+		<h2 class="text-2xl font-bold text-gray-700">O que é o ADN Politico?</h2>
+		<div class="space-y-2">
+			<p class="mt-2 text-lg text-gray-700">Iniciativa/Ferramenta apartidaria</p>
+			<p class="mt-2 text-lg text-gray-700">Entender semelhanças entre perfil político pessoal e a realidade das agendas dos partidos (tendência de voto)</p>
+			<p class="mt-2 text-lg text-gray-700">
+				Explicar o conceito de Plebiscito - porque é literalmente o que estamos a fazer, a simular uma "Democracia Direta" ao transferir o poder de voto do representante (deputado/partido) para o
+				eleitor.
+			</p>
+			<p class="mt-2 text-lg text-gray-700">Dicotomia entre o fácil e rápido acesso a informação hoje, mas dificuldade em "digerir" facilmente a informação Política em PT</p>
+			<p class="mt-2 text-lg text-gray-700">Consiste na simulação de voto em 15 medidas apresentadas na AR, com possibilidade de repetição personalizando os assuntos de voto</p>
+			<p class="mt-2 text-lg text-gray-700">(Link para o repositório de perguntas / open source code) -> acho importante abrir isto (perguntas, contra argumentos, tags, etc...)</p>
+			<p class="mt-2 text-lg text-gray-700">Explicar que os contra argumentos foram construídos por nós, com o nosso entendimento / interpretação da medida</p>
+			<p class="mt-2 text-lg text-gray-700">Possibilidade de partilhar com amigos interessados + ver o que outros votaram, mas aviso de que não guardamos dados individuais</p>
+			<p class="mt-2 text-lg text-gray-700">
+				Explicar o resultado final: Coincidência entre orientação de voto do partido, e do utilizador + Explicar que é possível entrar no detalhe da proposta através do link direto + ver facilmente em
+				tabela qual partido votou a favor, dando acesso a uma granularidade interessante de comparação não só na generalidade, mas para cada proposta individual
+			</p>
+		</div>
+	</div>
+	<div class="space-y-4">
+		<h2 class="text-2xl font-bold text-gray-700">Objetivos TLDR</h2>
+		<ul class="mt-4 list-inside list-disc space-y-2 text-lg text-gray-700">
+			<li>(i) mobilizar os eleitores portugueses para o voto</li>
+			<li>(ii) incentivo a um voto informado, munido de argumentos e fatos</li>
+			<li>(iii) auxílio na melhor caracterização e entendimento de cada agenda partidária política, através do registro histórico da sua tendência de voto na AR</li>
+		</ul>
+	</div>
+
+	<div class="space-y-4">
+		<h2 class="text-2xl font-bold text-gray-700">Quem somos?</h2>
+		<p class="text-lg text-gray-700">Insert texto Tiago a esquerda + Linkedin's pessoais?</p>
+	</div>
+
+	<div class="mt-8 space-y-4">
+		<h2 class="text-2xl font-bold text-gray-700">Como esta plataforma se distingue do clássico Quiz</h2>
+		<ul class="mt-4 list-inside list-disc space-y-2 text-lg text-gray-700">
+			<li>Possibilidade de deep dive em cada proposta</li>
+			<li>Objetividade da análise (vs subjetividade de uma "pergunta" num quiz)</li>
+			<li>Open Source</li>
+		</ul>
+	</div>
+
+	<div class="mx-auto max-w-7xl space-y-8 px-4 sm:px-6 lg:px-8">
+		<div class="mt-10 space-y-4 sm:mt-12 md:mt-16 lg:mt-20 xl:mt-28">
+			<h2 class="text-2xl font-extrabold tracking-tight text-gray-900 sm:text-3xl md:text-4xl">Falhas na metodologia</h2>
+			<ul class="mt-3 space-y-2 text-base text-gray-500 sm:mt-5 sm:text-lg md:mt-8">
+				<li class="mt-2">
+					<span class="text-gray-700">e.g. os contra argumentos podem estar biased</span>
+				</li>
+				<li class="mt-2">
+					<span class="text-gray-700">Apenas contém as que achamos "mais importantes" de 2023 - total 63</span>
+				</li>
+				<li class="mt-2">
+					<span class="text-gray-700">Apenas contém os "bullets de cada partido" das Legislativas de 2022 -> still to update</span>
+				</li>
+				<li class="mt-2">
+					<span class="text-gray-700">Retiramos as unânimes</span>
+				</li>
+			</ul>
+		</div>
+	</div>
+</div>
