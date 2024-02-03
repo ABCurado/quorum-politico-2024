55b0afec9c0fad416d49dcb719fc391efc53eb4d
tiagofurtado <48451189+tiagofurtado@users.noreply.github.com>
2024-02-03 11:39:32 +0000

    Update Welcome.svelte

--- src/routes/Welcome.svelte
+++ src/routes/Welcome.svelte
@@ -16,22 +16,22 @@
 		<!-- <h1 class="text-center text-4xl mb-4">Descobre em quem votar</h1> -->
 		<!--Olá! Bem-vindo ao <strong>Concordas?</strong>, um jogo que te permite descobrir em quem votar nas próximas eleições. O jogo é baseado nas votaçõoes que aconteceram no parlamento europeu de 2022-2023. -->
 
-		<h1 class="text-center text-4xl mb-4">Descobre o teu ADN Político!</h1>
-		<p class="text-center text sm:text-base mb-4">Pronuncia-te diretamente sobre as propostas de legislação e defende os teus interesses no parlamento. Sê o teu próprio representante partidário. Vê que partido melhor te representou.</p>
+		<h1 class="text-center text-4xl mb-4"><strong>ADN Político</strong> - Descobre que partido melhor te representa.</h1>
+		<p class="text-center text sm:text-base mb-4">Defende os <strong>teus</strong> interesses no parlamento. Sê o teu próprio representante partidário.</p>
 		<!-- {#key ticker} -->
 		<Hemicycle centerText="" random={true} />
 		<!-- {/key} -->
 
 		<div class="mb-8 mt-4">
-			As <b>instruções</b> são simples:
+			<!--As <b>instruções</b> são simples:-->
 			<ul>
 				<li class="ml-3 flex items-center">
 					<IconCheckbox size={16} stroke={3} color="green" />
-					<span class="ml-2">Lê a proposta que te é apresentada, sumário, riscos e críticas.</span>
+					<span class="ml-2">Lê o sumário, riscos e críticas de cada proposta que te é apresentada.</span>
 				</li>
 				<li class="ml-3 flex items-center">
 					<IconCheckbox size={16} stroke={3} color="green" />
-					<span class="ml-2">Usa os poderes do partido: vota a favor, contra, ou abstem-te.</span>
+					<span class="ml-2">Decide se és a favor, contra ou simplesmente abstem-te.</span>
 				</li>
 				<li class="ml-3 flex items-center">
 					<IconCheckbox size={16} stroke={3} color="green" />
