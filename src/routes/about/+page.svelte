e10fca142f64ceeb32a6056ee1e90b4b6c9bdb4f
tiagofurtado <48451189+tiagofurtado@users.noreply.github.com>
2024-02-04 13:53:01 +0000

    Update +page.svelte

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -34,7 +34,7 @@
 	</div>
 
 	<div class="space-y-4">
-		<h2 class="text-2xl font-bold text-gray-700">Para sugestões, ideias ou reclamações:</h2>
+		<h2 class="text-2xl font-bold text-gray-700">Para sugestões, elogios ou reclamações:</h2>
 		<p class="text-lg text-gray-700"><a href="https://twitter.com/ab_curado" target="_blank">twitter.com/ab_curado</a></p>
 		<p class="text-lg text-gray-700"><a href="https://twitter.com/RicardoPinho96" target="_blank">twitter.com/RicardoPinho96</a></p>
 		<p class="text-lg text-gray-700"><a href="https://twitter.com/tiagomfurtado" target="_blank">twitter.com/tiagomfurtado</a></p>
