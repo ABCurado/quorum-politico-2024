c7440c9676c84dd54672fce4269ee018694ce151
tiagofurtado <48451189+tiagofurtado@users.noreply.github.com>
2024-02-04 14:00:38 +0000

    Update +page.svelte

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -7,30 +7,13 @@
 </div>
 <div class="relative mx-auto w-10/12 space-y-8 py-3 sm:mx-auto">
 	<div class="space-y-4">
-		<h2 class="text-2xl font-bold text-gray-700">O que é o ADN Politico?</h2>
+		<h2 class="text-2xl font-bold text-gray-700">ADN Político:</h2>
 		<div class="space-y-2">
-			<p class="mt-2 text-lg text-gray-700">Iniciativa/Ferramenta apartidaria</p>
-			<p class="mt-2 text-lg text-gray-700">Entender semelhanças entre perfil político pessoal e a realidade das agendas dos partidos (tendência de voto)</p>
-			<p class="mt-2 text-lg text-gray-700">Explicar o conceito de Plebiscito - porque é literalmente o que estamos a fazer, a simular uma "Democracia Direta" ao transferir o poder de voto do representante (deputado/partido) para o
-				eleitor.</p>
-			<p class="mt-2 text-lg text-gray-700">Dicotomia entre o fácil e rápido acesso a informação hoje, mas dificuldade em "digerir" facilmente a informação Política em PT</p>
-			<p class="mt-2 text-lg text-gray-700">Consiste na simulação de voto em 15 medidas apresentadas na AR, com possibilidade de repetição personalizando os assuntos de voto</p>
-			<p class="mt-2 text-lg text-gray-700">(Link para o repositório de perguntas / open source code) -> acho importante abrir isto (perguntas, contra argumentos, tags, etc...)</p>
-			<p class="mt-2 text-lg text-gray-700">Explicar que os contra argumentos foram construídos por nós, com o nosso entendimento / interpretação da medida</p>
-			<p class="mt-2 text-lg text-gray-700">Possibilidade de partilhar com amigos interessados + ver o que outros votaram, mas aviso de que não guardamos dados individuais</p>
-			<p class="mt-2 text-lg text-gray-700">
-				Explicar o resultado final: Coincidência entre orientação de voto do partido, e do utilizador + Explicar que é possível entrar no detalhe da proposta através do link direto + ver facilmente em
-				tabela qual partido votou a favor, dando acesso a uma granularidade interessante de comparação não só na generalidade, mas para cada proposta individual
-			</p>
+			<p class="mt-2 text-lg text-gray-700">Iniciativa apartidária para votares diretamente em propostas parlamentares e perceberes que partido melhor te representa. 
+								Ignoramos agendas políticas e partidárias, o que se diz em entrevistas, o que se escreve e promete em programas eleitoriais. 
+								Queremos avaliar objetivamente como cada partido utiliza o seu poder de voto.
+								Melhorando a transparência sobre a atividade parlamentar conseguimos contribuir para interações políticas menos populistas e polarizadas.</p>
 		</div>
-	</div>
-	<div class="space-y-4">
-		<h2 class="text-2xl font-bold text-gray-700">Objetivos TLDR</h2>
-		<ul class="mt-4 list-inside list-disc space-y-2 text-lg text-gray-700">
-			<li>(i) mobilizar os eleitores portugueses para o voto</li>
-			<li>(ii) incentivo a um voto informado, munido de argumentos e fatos</li>
-			<li>(iii) auxílio na melhor caracterização e entendimento de cada agenda partidária política, através do registro histórico da sua tendência de voto na AR</li>
-		</ul>
 	</div>
 
 	<div class="space-y-4">
