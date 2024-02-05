5c94bbe7c1ae9a4974a73827a23b1456c555d927
sem-politiquices <154367938+sem-politiquices@users.noreply.github.com>
2024-02-05 23:28:24 +0100

    Update Document.svelte

--- src/routes/Document.svelte
+++ src/routes/Document.svelte
@@ -106,8 +106,13 @@ function transformBulletsToHTML(text) {
 		</div>
 
 		<h3 class="mb-2 text-xl font-semibold text-gray-800 sm:text-2xl flex items-center">
-			Riscos e Críticas <a href="https://forms.gle/kT4ktnhZtRppQVon8" target="_blank" class="text-gray-400 ml-1"><IconHelpHexagonFilled size={22}/></a>:
+		    Riscos e Críticas: 
+		    <a href="https://forms.gle/kT4ktnhZtRppQVon8" target="_blank" class="text-gray-400 ml-1"><IconHelpHexagonFilled size={22}/>
+		        
+		        <span class="ml-1">Esquecemo-nos de algum?</span>
+		    </a>:
 		</h3>
+
 		<div class="mb-6 min-h-52 w-full max-w-4xl rounded-md bg-white p-4 text-base shadow-md sm:text-lg">
 			<!-- Your content here -->
 
