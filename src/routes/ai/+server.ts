0899dd2edf204f190b966cf361d5618becb4cebb
Antonio Curado <abestevao@gmail.com>
2024-02-08 22:13:25 +0100

    Fix import and update AI model usage

--- src/routes/ai/+server.ts
+++ src/routes/ai/+server.ts
@@ -1,18 +1,18 @@
-import { Ai } from "@cloudflare/ai";
+import { Ai } from '@cloudflare/ai';
 import type { RequestHandler } from '@sveltejs/kit';
 
 // A get route that returns information about a specific party
 /** @type {import('@sveltejs/kit').RequestHandler} */
 export const GET: RequestHandler = async ({ platform, url }) => {
-    const ai = new Ai(platform?.env.AI);
-    const inputs = {
-      // text: "I don't like you. I hate you",
-      text: 'I like you. I love you.'
-    };
-
-    const response = await ai.run(
-      '@cf/huggingface/distilbert-sst-2-int8',
-      inputs
-    );
-    return new Response(JSON.stringify(inputs,response), { headers: { 'Content-Type': 'application/json' } });
+	const ai = new Ai(platform?.env.AI);
+	// messages - chat style input
+	let chat = {
+		messages: [
+			{ role: 'system', content: 'You are a helpful assistant.' },
+			{ role: 'user', content: 'Who won the world series in 2020?' }
+		]
+	};
+	let response = await ai.run('@cf/meta/llama-2-7b-chat-int8', chat);
+	console.log('response.outputs', response);
+	return new Response(JSON.stringify(chat, response), { headers: { 'Content-Type': 'application/json' } });
 };
