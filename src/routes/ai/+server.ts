da4ca1b0b2e0e756cbe148a7e143b7361ce6546b
Antonio Curado <abestevao@gmail.com>
2024-02-08 22:25:31 +0100

    Update AI chat messages

--- src/routes/ai/+server.ts
+++ src/routes/ai/+server.ts
@@ -4,15 +4,17 @@ import type { RequestHandler } from '@sveltejs/kit';
 // A get route that returns information about a specific party
 /** @type {import('@sveltejs/kit').RequestHandler} */
 export const GET: RequestHandler = async ({ platform, url }) => {
+    let party = url.searchParams.get('party');
+
 	const ai = new Ai(platform?.env.AI);
 	// messages - chat style input
 	let chat = {
 		messages: [
-			{ role: 'system', content: 'You are a helpful assistant.' },
-			{ role: 'user', content: 'Who won the world series in 2020?' }
+			{ role: 'system', content: 'You are a helpful comical assistant that knows about portuguese politics. I will send you a sentiment and a political party and you have to reply a sentente. The response should be a single sentence in Portuguese from Portugal' },
+			{ role: 'user', content: `Funny ${party}` }
 		]
 	};
 	let response = await ai.run('@cf/meta/llama-2-7b-chat-int8', chat);
 	console.log('response.outputs', response);
-	return new Response(JSON.stringify(response, ), { headers: { 'Content-Type': 'application/json' } });
+	return new Response(JSON.stringify(response), { headers: { 'Content-Type': 'application/json' } });
 };
