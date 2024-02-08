2d5f9605484293a5086023ace0267a291df63d4f
tiagofurtado <48451189+tiagofurtado@users.noreply.github.com>
2024-02-08 23:38:23 +0000

    Update BarChart.svelte

--- src/routes/BarChart.svelte
+++ src/routes/BarChart.svelte
@@ -12,6 +12,7 @@
 	};
 </script>
 
+<!--
 <div class="w-2/3 lg:w-1/2 xl:w-1/3">
 	{#each proximity as party}
 		<div class="flex items-center justify-between">
@@ -23,8 +24,9 @@
 		</div>
 	{/each}
 </div>
+-->
 
-<!--
+
 <div class="w-2/3 lg:w-1/2 xl:w-1/3">
     {#each proximity as party}
         <div class="flex items-center justify-between">
@@ -38,4 +40,4 @@
         </div>
     {/each}
 </div>
--->
+
