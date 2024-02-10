15eb1e5b3b2a1834109eb185408e03b9e84f3d77
Antonio Curado <abestevao@gmail.com>
2024-02-10 14:01:52 +0100

    Refactor AI module and update party description function

--- src/routes/ai/ai.ts
+++ src/routes/ai/ai.ts
@@ -1,19 +1,19 @@
-// import { Ai } from '@cloudflare/ai';
+import { Ai } from '@cloudflare/ai';
 
-// export async function randomPartyDescription(platform: any, party: string) {
-//     const ai = new Ai(platform?.env.AI);
-//     // messages - chat style input
-//     let chat = {
-//         messages: [
-//             { role: 'system', content: 'You are a helpful comical assistant that knows about portuguese politics. I will send you a sentiment and a political party and you have to reply a sentente. The response should be a single sentence in Portuguese from Portugal' },
-//             { role: 'user', content: `Funny PS` }
-//         ]
-//     };
-//     let response = await ai.run('@cf/meta/llama-2-7b-chat-int8', chat);
-//     console.log('response', response);
-//     // Do something with the response
-//     return response.response;
-// }
+export async function randomPartyDescription(platform: any, party: string) {
+    const ai = new Ai(platform?.env.AI);
+    // messages - chat style input
+    let chat = {
+        messages: [
+            { role: 'system', content: 'You are a helpful comical assistant that knows about portuguese politics. I will send you a sentiment and a political party and you have to reply a sentente. The response should be a single sentence in Portuguese from Portugal' },
+            { role: 'user', content: `Funny PS` }
+        ]
+    };
+    let response = await ai.run('@cf/meta/llama-2-7b-chat-int8', chat);
+    console.log('response', response);
+    // Do something with the response
+    return response.response;
+}
 
 export async function randomPolicialSentence(platform: any) {
     const ai = new Ai(platform?.env.AI);
