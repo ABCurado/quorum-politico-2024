cced5931657d068b974807521c5982011969329b
Antonio Curado <abestevao@gmail.com>
2024-02-10 14:48:26 +0100

    Add GET and POST routes for AI server

--- src/routes/ai/+server.ts
+++ src/routes/ai/+server.ts
@@ -1,10 +1,24 @@
+import { aiPersonaSummary, randomPolicialSentence } from './ai';
+
 import type { RequestHandler } from '@sveltejs/kit';
-import { randomPolicialSentence } from './ai';
 
 // A get route that returns information about a specific party
 /** @type {import('@sveltejs/kit').RequestHandler} */
 export const GET: RequestHandler = async ({ platform, url }) => {
     let response = await randomPolicialSentence(platform);
-    console.log('response.outputs', response);
+
     return new Response(JSON.stringify({"response": response}), { headers: { 'Content-Type': 'application/json' } });
 };
+
+/** @type {import('@sveltejs/kit').RequestHandler} */
+export const POST: RequestHandler = async ({ request,  platform, url }) => {
+    let body = await request.json();
+
+    let proposals = body.proposals;
+    let winningPartyShortDescription = body.winningPartyShortDescription;
+
+    let response = await aiPersonaSummary(platform, proposals, winningPartyShortDescription);
+
+    return new Response(JSON.stringify({"response": response}), { headers: { 'Content-Type': 'application/json' } });
+};
+
