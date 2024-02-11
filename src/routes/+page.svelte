3e0d60bb382819767f9c82509d445295f3c063a1
tiagofurtado <48451189+tiagofurtado@users.noreply.github.com>
2024-02-11 17:36:17 +0000

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -45,29 +45,19 @@
 	}
 
 	$: if (currentVote === quizSize) {
-		let partyProximity = { BE: 0, CH: 0, IL: 0, L: 0, PAN: 0, PCP: 0, PS: 0, PSD: 0 };
+		let partyProximity = { "BE": 0, "CH": 0, "IL": 0, "L": 0, "PAN": 0, "PCP": 0, "PS": 0, "PSD": 0 };
 
-		/*
 		for (let proposal of data.db) {
 			for (const [party, result] of Object.entries(proposal.votes)) {
 				if (result == proposal.user_vote) {
 					partyProximity[party] += 1;
-				} else if (proposal.user_vote == 2) {
-					partyProximity[party] += 0.5;
-				} else {
-					partyProximity[party] += 0;
-				}
-			}
-		}
-		*/
-		
-		for (let proposal of data.db) {
-			for (const [party, result] of Object.entries(proposal.votes)) {
-				if (result == proposal.user_vote) {
-					partyProximity[party] += 1;
-				} else if ((result == 0 && proposal.user_vote == 1) || (result == 1 && proposal.user_vote == 0)) {
+				} else if (result == 0 && proposal.user_vote == 1) {
 					partyProximity[party] -= 1;
-				} else if ((result == 1 && proposal.user_vote == 2) || (result == 2 && proposal.user_vote == 1)) {
+				} else if (result == 1 && proposal.user_vote == 0) {
+					partyProximity[party] -= 1;
+				} else if (result == 2 && proposal.user_vote == 1) {
+					partyProximity[party] += 0.5;
+				} else if (result == 1 && proposal.user_vote == 2) {
 					partyProximity[party] += 0.5;
 				} else {
 					partyProximity[party] += 0;
