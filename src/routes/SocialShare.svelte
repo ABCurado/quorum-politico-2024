1d914221021ab37e576ef07d32ce03f1aa79ec88
Antonio Curado <abestevao@gmail.com>
2024-02-11 21:27:20 +0100

    Refactor social share button

--- src/routes/SocialShare.svelte
+++ src/routes/SocialShare.svelte
@@ -45,20 +45,24 @@
 	}
 </script>
 
-<button class="share-button flex cursor-pointer items-center rounded-full border-2 bg-gray-200 bg-opacity-30 px-4 py-4 shadow-lg hover:shadow-2xl" on:click={() => navigatorShare()}>
-	<IconShare size={48} stroke={2} class="text-sky-500 opacity-75" />
-</button>
-<!-- <Email subject={title} body="{desc} {url}" /> -->
-<!-- <HackerNews class="share-button" {title} {url} /> -->
-<!-- <Reddit class="share-button" {title} {url} /> -->
-<LinkedIn class="share-button rounded" {url} />
-<!-- <Tumblr class="share-button" {title} {url} caption={title} /> -->
-<!-- <Pinterest class="share-button" {url} media="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Svelte_Logo.svg/200px-Svelte_Logo.svg.png" description={title} /> -->
-<Telegram class="share-button rounded" text={title} - {desc} {url} />
-<!-- <Vk class="share-button" {title} {url} /> -->
-<WhatsApp class="share-button rounded" text="{title} {url}" />
-<!-- <Xing class="share-button" {title} {url} /> -->
-<Facebook class="share-button rounded" quote={title} {url} />
-<X class="share-button rounded" text={title} {desc} {url} related="other,users" {hashtags} />
-<!-- <Line class="share-button" {url} /> -->
-<!-- <button class="share-button rounded" on:click={() => window.navigator.clipboard.writeText(url)}>Copy Link</button> -->
+
+{#if supportsNavigatorShare}
+	<button class="share-button flex cursor-pointer items-center rounded-full border-2 bg-gray-200 bg-opacity-30 px-4 py-4 shadow-lg hover:shadow-2xl" on:click={() => navigatorShare()}>
+		<IconShare size={48} stroke={2} class="text-sky-500 opacity-75" />
+	</button>
+{:else}
+	<!-- <Email subject={title} body="{desc} {url}" /> -->
+	<!-- <HackerNews class="share-button" {title} {url} /> -->
+	<!-- <Reddit class="share-button" {title} {url} /> -->
+	<LinkedIn class="share-button rounded" {url} />
+	<!-- <Tumblr class="share-button" {title} {url} caption={title} /> -->
+	<!-- <Pinterest class="share-button" {url} media="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Svelte_Logo.svg/200px-Svelte_Logo.svg.png" description={title} /> -->
+	<Telegram class="share-button rounded" text={title} - {desc} {url} />
+	<!-- <Vk class="share-button" {title} {url} /> -->
+	<WhatsApp class="share-button rounded" text="{title} {url}" />
+	<!-- <Xing class="share-button" {title} {url} /> -->
+	<Facebook class="share-button rounded" quote={title} {url} />
+	<X class="share-button rounded" text={title} {desc} {url} related="other,users" {hashtags} />
+	<!-- <Line class="share-button" {url} /> -->
+	<!-- <button class="share-button rounded" on:click={() => window.navigator.clipboard.writeText(url)}>Copy Link</button> -->
+{/if}
