ba4926d4a01c3dc2a5a0b766e88f14d56d835595
tiagofurtado <48451189+tiagofurtado@users.noreply.github.com>
2024-02-11 18:43:36 +0000

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -49,19 +49,13 @@
 
 		for (let proposal of data.db) {
 			for (const [party, result] of Object.entries(proposal.votes)) {
-				if (result == proposal.user_vote) {
-					partyProximity[party] += 1;
-				} else if (result == 0 && proposal.user_vote == 1) {
-					partyProximity[party] += -1;
-				} else if (result == 1 && proposal.user_vote == 0) {
-					partyProximity[party] += -1;
-				} else if (result == 2 && proposal.user_vote == 1) {
-					partyProximity[party] += 0.5;
-				} else if (result == 1 && proposal.user_vote == 2) {
-					partyProximity[party] += 0.5;
-				} else {
-					partyProximity[party] += 0;
-				}
+				if (result === proposal.user_vote) {
+            				partyProximity[party]++;
+        			} else if ((result === 0 && proposal.user_vote === 1) || (result === 1 && proposal.user_vote === 0)) {
+            				partyProximity[party]--;
+        			} else if ((result === 2 && proposal.user_vote === 1) || (result === 1 && proposal.user_vote === 2)) {
+            				partyProximity[party] += 0.5;
+        			}
 			}
 		}
 
