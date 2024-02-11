001e3dbf132d84a812a152ccd33efeb9decaf5e4
tiagofurtado <48451189+tiagofurtado@users.noreply.github.com>
2024-02-11 19:14:42 +0000

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -54,7 +54,7 @@
         			} else if ((result == 0 && proposal.user_vote == 1) || (result == 1 && proposal.user_vote == 0)) {
             				partyProximity[party]--;
         			} else if ((result == 2 && proposal.user_vote == 1) || (result == 1 && proposal.user_vote == 2)) {
-            				partyProximity[party] += 0,5;
+            				partyProximity[party] += 0.5;
         			}
 			}
 		}
