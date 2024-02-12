60b4cc30ce299ce134b5736d4e4ee3c42a74aa6a
tiagofurtado <48451189+tiagofurtado@users.noreply.github.com>
2024-02-12 09:40:11 +0000

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -64,7 +64,7 @@
         			} else if ((result == 2 && proposal.user_vote == 1) || (result == 1 && proposal.user_vote == 2)) {
             				partyProximity[party] += 0.5;
         			} else if ((result == 2 && proposal.user_vote == 0) || (result == 0 && proposal.user_vote == 2)) {
-            				partyProximity[party] -= 0;
+            				partyProximity[party] += 0;
         			}
 			}
 		}
