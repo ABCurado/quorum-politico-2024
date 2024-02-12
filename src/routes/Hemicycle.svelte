da4559742b212aaf10f76ed2225092a0219d6702
tiagofurtado <48451189+tiagofurtado@users.noreply.github.com>
2024-02-12 17:49:05 +0000

    Update Hemicycle.svelte

--- src/routes/Hemicycle.svelte
+++ src/routes/Hemicycle.svelte
@@ -57,10 +57,10 @@
 
 		<!-- Created with the Wikimedia parliament diagram creator (http://tools.wmflabs.org/parliamentdiagram/parliamentinputform.html) -->
 		<g>
-			<!--{#if !random} -->
-				<!-- <text x="175" y="175" class="text-center text-3xl font-bold" style="text-anchor:middle;"> {centerText} </text> -->
-				<img src={logo}/>
-			<!-- {/if} -->
+			{#if !random}
+				<text x="175" y="175" class="text-center text-3xl font-bold" style="text-anchor:middle;"> {centerText} </text>
+				<!--<img src={logo}/>-->
+			{/if}
 
 			{#each parlimentData as seat}
 				{@const maxOpacity = calculateOpacity(partyRanking[seat[2]])}
