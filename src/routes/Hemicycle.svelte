925d8a8f4ba338dcaff4854fddb59c34692c38da
tiagofurtado <48451189+tiagofurtado@users.noreply.github.com>
2024-02-12 18:02:53 +0000

    Update Hemicycle.svelte

--- src/routes/Hemicycle.svelte
+++ src/routes/Hemicycle.svelte
@@ -48,7 +48,7 @@
 		<!-- Created with the Wikimedia parliament diagram creator (http://tools.wmflabs.org/parliamentdiagram/parliamentinputform.html) -->
 		<g>
 			{#if !random}
-				<text x="175" y="175" class="text-center text-3xl font-bold" style="text-anchor:middle;"> {centerText} </text>
+				<text x="180" y="175" class="text-center text-3xl font-bold" style="text-anchor:middle;"> {centerText} </text>
 			{/if}
 
 			{#each parlimentData as seat}
