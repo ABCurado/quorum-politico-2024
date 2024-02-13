eb7e36626c100db9c245a88c811040e7add033ba
tiagofurtado <48451189+tiagofurtado@users.noreply.github.com>
2024-02-13 11:54:42 +0000

    Update Hemicycle.svelte

--- src/routes/Hemicycle.svelte
+++ src/routes/Hemicycle.svelte
@@ -51,7 +51,7 @@
 			{#if !random}
 				<!-- <text x="180" y="175" class="text-center text-3xl font-bold" style="text-anchor:middle;"> {centerText} </text> -->
 				<!-- href="https://developer.mozilla.org/en-US/docs/Web/SVG/Element/image/mdn_logo_only_color.png" -->
-				<image href="/psd.png" x="160" y="130" width="50" height="50" />
+				<image href="/psd.png" x="155" y="130" width="50" height="50" />
 			{/if}
 
 			{#each parlimentData as seat}
