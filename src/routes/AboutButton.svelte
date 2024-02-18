cd2181ff6782890724e38b553e543d7c7c027105
sem-politiquices <154367938+sem-politiquices@users.noreply.github.com>
2024-02-18 12:25:03 +0100

    Update AboutButton.svelte

--- src/routes/AboutButton.svelte
+++ src/routes/AboutButton.svelte
@@ -4,7 +4,7 @@
 
 <a
 	href="/about"
-	class="m-7 flex items-center rounded-full border border-slate-300 bg-opacity-50 p-4 text-sm font-medium text-slate-600 shadow-inner transition-colors duration-300 ease-in-out hover:bg-slate-200"
+	class="m-7 flex items-center rounded-full border border-slate-300 bg-opacity-50 p-4 text-sm font-medium text-slate-600 shadow-inner transition-colors duration-300 ease-in-out hover:bg-slate-200 focus:border-black-500"
 >
 	<IconUserQuestion class="icon-pulse" size={24} />
 </a>
