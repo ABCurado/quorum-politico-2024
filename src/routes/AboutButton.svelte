f91ef6735cd5c4c8315a23a5717ac61f080df9de
sem-politiquices <154367938+sem-politiquices@users.noreply.github.com>
2024-02-18 12:28:44 +0100

    Update AboutButton.svelte

--- src/routes/AboutButton.svelte
+++ src/routes/AboutButton.svelte
@@ -4,7 +4,7 @@
 
 <a
 	href="/about"
-	style="outline: 3px solid black;" <!-- Adjust the color and thickness as needed -->
+	style="outline: 3px solid black;"
 	class="m-7 flex items-center rounded-full border border-slate-300 bg-opacity-50 p-4 text-sm font-medium text-slate-600 shadow-inner transition-colors duration-300 ease-in-out hover:bg-slate-200"
 >
 	<IconUserQuestion class="icon-pulse" size={24} />
