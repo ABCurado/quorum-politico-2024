aa5b75ed9d07a3fc858200ff832a5e82ce2a522a
sem-politiquices <154367938+sem-politiquices@users.noreply.github.com>
2024-02-18 12:21:37 +0100

    Update +page.svelte

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -64,7 +64,7 @@
 
 	<div class="space-y-4">
 		<h2 class="text-2xl font-bold text-gray-700">Agradecimentos:</h2>
-		<p class="text-sm text-gray-700">Maria R. Gonçalves, Rui Maciel, Catarina Ribero Lopes, Pedro Gonzalez, Bernardo Peixoto Silva</p>
+		<p class="text-sm text-gray-700">Maria R. Gonçalves, Matilde P. Aires, Rui Maciel, Catarina Ribeiro Lopes, Pedro Gonzalez, Bernardo Peixoto Silva</p>
 	</div>
 
 </div>
