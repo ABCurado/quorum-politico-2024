1d39a52f4368ce3a36b29aa75e2379ec112b58af
Antonio Curado <abestevao@gmail.com>
2024-02-25 21:59:21 +0100

    Refactor proposal filtering and pagination logic

--- src/routes/proposals/+server.ts
+++ src/routes/proposals/+server.ts
@@ -8,27 +8,26 @@ import db from './proposals_db.json';
 export const GET: RequestHandler = async ({ request, platform, url }) => {
 	const tags = url.searchParams.get('tags')?.split(',') ?? [];
 	const pageSize: number = Number(url.searchParams.get('pageSize') ?? 10);
-
+	const excludeIds: string[] = url.searchParams.get('exlcudeIds')?.split(",") ?? [];
+	console.log('excludeIds', excludeIds);
 	// Filter proposals atribute by tags
-	let filteredProposals = db.filter((proposal) => {
-		return tags.some((tag: string) => {
-			return proposal.tag_1 === tag;
+	let filteredProposals = db
+	if (tags.length > 0) {
+		filteredProposals = filteredProposals.filter((proposal) => {
+			return tags.some((tag: string) => {
+				return proposal.tag_1 === tag;
+			});
 		});
+	}
+	// Exclude proposals by id
+	filteredProposals = filteredProposals.filter((proposal) => {
+		return !excludeIds.includes(proposal.official_id);
 	});
-
 	// Randomize the order of filtered proposals
 	const randomizedProposals = filteredProposals.sort(() => Math.random() - 0.5);
 	// Paginate the randomized proposals
-	let paginatedProposals = randomizedProposals.slice(0, pageSize - 1);
-	const easter_egg = db.filter((proposal) => {
-		// Use the Proposal type
-		return tags.some((tag: string) => {
-			return proposal.official_id === '3/XV-2';
-		});
-	});
-
-	paginatedProposals = [...paginatedProposals, ...easter_egg];
-
+	let paginatedProposals = randomizedProposals.slice(0, pageSize);
+	
 	return new Response(
 		JSON.stringify({
 			proposals: paginatedProposals,
